64a9e0eea49251119f4b30ea5d57cae22d3e47dd
Omid Alavi <somidalavi@aut.ac.ir>
2020-06-07 23:40:23 +0430

    added a simple playlist also it now displays artist name,title album and song duration'

--- Gui.py
+++ Gui.py
@@ -2,13 +2,14 @@ from PySide2 import QtCore, QtWidgets, QtGui
 from PySide2.QtCore import Slot
 from PySide2.QtMultimedia import QMediaPlayer
 from Model import main_model
-import threading
+from functools import partial
 import pathlib
+import threading
 
-file_path = str(pathlib.Path(__file__).parent.absolute());
-print("current path is " + file_path)
+file_path = pathlib.Path(__file__).parent.absolute();
+print(type(file_path))
 
-update_interval = 500
+update_interval = 100
 slider_range = [1,20000]
 slider_page_step  = 2000;
 slider_single_step = 700;
@@ -21,7 +22,7 @@ class SlidersAndNameWidget(QtWidgets.QWidget):
         
         self._update_timer = QtCore.QTimer();
         self._update_timer.setInterval(update_interval)
-        self._update_timer.timeout.connect(self.update_slider);
+        self._update_timer.timeout.connect(self.update);
         self._update_timer.start(update_interval)
 
         self._volume_slider = QtWidgets.QSlider(QtCore.Qt.Orientation.Vertical)
@@ -29,6 +30,9 @@ class SlidersAndNameWidget(QtWidgets.QWidget):
         self._volume_slider.setValue(50);
 
         self._song_name_label = QtWidgets.QLabel("No Song");
+        self._model.player.currentMediaChanged.connect(self.update_label)
+
+        self._duration_label = QtWidgets.QLabel("00:00 / 00:00");
         
         self._player_slider = QtWidgets.QSlider(QtCore.Qt.Orientation.Horizontal);
         self._player_slider.valueChanged.connect(self.value_change);
@@ -41,6 +45,7 @@ class SlidersAndNameWidget(QtWidgets.QWidget):
 
         temp_layout = QtWidgets.QVBoxLayout()
         temp_layout.addWidget(self._song_name_label);
+        temp_layout.addWidget(self._duration_label)
         temp_layout.addWidget(self._player_slider);
         temp_widget = QtWidgets.QWidget();
         temp_widget.setLayout(temp_layout);
@@ -50,25 +55,33 @@ class SlidersAndNameWidget(QtWidgets.QWidget):
         self._layout.addWidget(self._volume_slider);
         
         self.setLayout(self._layout)
-        
+    def update_label(self):
+        media = self._model.get_current_media_data();
+        self._song_name_label.setText(media.title);
+        self.update()
     def dragging(self):
         self._update_timer.stop()
     def released(self):
         self._update_timer.setInterval(update_interval)
         self._update_timer.start(update_interval)
 
-    def update_slider(self):
-
-        print(threading.activeCount())
+    def update(self):
+        pos = self._model.get_position()
+        if pos is None : return 
+        #update the sliders
         self.test = True;
-        self._player_slider.setValue(int(self._model.get_position()*  slider_range[1])+ 1 );
+        self._player_slider.setValue(int(pos*slider_range[1])+ 1 );
         self.test = False;
-    
+        # set the duration label correctoy
+        mdata = self._model.get_current_media_data();
+        passed = int(pos * mdata.duration);
+        total = mdata.duration;
+        self._duration_label.setText("%02d:%02d / %02d:%02d" % (passed // 60 ,passed % 60,
+                                                       total // 60 , total % 60));
     def volume_change(self,value):
         self._model.set_volume(value)
     
     def value_change(self):
-        print("dfsd " + threading.current_thread().getName())
         if self.test: return 
         val = self._player_slider.value();
         self._model.seek(val / slider_range[1]);
@@ -82,13 +95,14 @@ class MediaButtonsWidget(QtWidgets.QWidget):
         self._buttons = []
         self._layout = QtWidgets.QHBoxLayout()
         for icon_name,func in zip(icon_names,button_clicked_funcs):
-            icon = QtGui.QIcon(file_path + '/icons/' + icon_name);
+            icon = QtGui.QIcon(str(file_path / 'icons' / icon_name));
             button = QtWidgets.QPushButton();
             button.clicked.connect(func)
             button.setIcon(icon);
             self._buttons.append(button);
             self._layout.addWidget(button);
         self.setLayout(self._layout);
+
     def rewind(self):
         pass
     def play(self):
@@ -98,9 +112,11 @@ class MediaButtonsWidget(QtWidgets.QWidget):
         print("Pausing")
         self._model.pause()
     def stop(self):
-        pass
+        self._model.stop()
     def forwards(self):
-        pass
+        self._model.forwards()
+    def rewind(self):
+        self._model.rewind()
 
 
 class MediaPlayerWidget(QtWidgets.QWidget):
@@ -114,30 +130,48 @@ class MediaPlayerWidget(QtWidgets.QWidget):
         self._layout.addWidget(self._media_buttons);
         self.setLayout(self._layout);
 
-class PlayListWidget(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__();
-
 class GuiHelper:
     @staticmethod
-    def open_file(): 
-        print(threading.activeCount())
+    def open_file(pl_widget_h): 
         fileName = QtWidgets.QFileDialog.getOpenFileName(None,
-        "Open Song", "~", "Audio Files (*.mp3 *.wav)")
-        print("HRe")
-        if len(fileName) == 0 : return
-        main_model.open_file(fileName[0]);
+        "Choose Song", "~", "Audio Files (*.mp3 *.wav *.m4a)")
+        if len(fileName[0]) == 0 : return
+        current_playlist = pl_widget_h.get_current_pl_name()
+        index = main_model.add_file(fileName[0],current_playlist);
+        print("Got it in Index " + str(index))
+        main_model.open_file(index,current_playlist);
+        pl_widget_h.add_song_to_cur_playlist(main_model.get_current_media_data())
 
 class MenuBarWidget(QtWidgets.QMenuBar):
-    def __init__(self):
+    def __init__(self,pl_widget_h):
         super().__init__();
         self._model = main_model
         self._file_menu = QtWidgets.QMenu(self);
         self._file_menu.setTitle("File");
         self._open_action = self._file_menu.addAction("Open File");
         self._open_action.setShortcuts(QtGui.QKeySequence.Open)
-        self._open_action.triggered.connect(GuiHelper.open_file);
+        self._open_action.triggered.connect(partial(GuiHelper.open_file,pl_widget_h));
         self.addMenu(self._file_menu)
+
+class PlayListWidget(QtWidgets.QTabWidget):
+    def __init__(self):
+        super().__init__();
+        self._model = main_model
+        now_playing = QtWidgets.QListWidget();
+        self.addTab(now_playing,"Now Playing");
+        now_playing.currentRowChanged.connect(self.song_index_changed);
+        self._model.add_playlist("Now Playing");
+        
+    def add_song_to_cur_playlist(self,media):
+        print(type(media))
+        self.currentWidget().addItem(QtWidgets.QListWidgetItem(media.title));
+    def song_index_changed(self,index):
+        print("In Index " + str(index))
+        self._model.open_file(index,self.get_current_pl_name());
+
+    def get_current_pl_name(self):
+        return self.tabText(self.currentIndex())
+
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
 
@@ -145,22 +179,21 @@ class MainWindow(QtWidgets.QWidget):
         self.setWindowTitle("Music Player");
         self._model = main_model;
         
-        #TODO: add a status bar and menu bar
-        self._menu = MenuBarWidget();
-        
-
-        self._status = QtWidgets.QStatusBar();
-        self._status.showMessage("Stopped");
-        def update_status(status):
-            if status == QMediaPlayer.StoppedState: self._status.showMessage("Stopped")
-            elif status == QMediaPlayer.PausedState : self._status.showMessage("Paused")
-            else : self._status.showMessage("Playing")
-
-        self._model.player.stateChanged.connect(update_status);
-        
-        self._player = MediaPlayerWidget()
         self._playlist = PlayListWidget()
         
+        #TODO: add a status bar and menu bar
+        self._menu = MenuBarWidget(self._playlist);
+        
+        self._status = QtWidgets.QStatusBar();
+        self._status.showMessage("Stopped");
+        def update_title(status):
+            if status == QMediaPlayer.StoppedState: self.setWindowTitle("Music Player-Stopped")
+            elif status == QMediaPlayer.PausedState : self.setWindowTitle("Music Player-Paused")
+            else : self.setWindowTitle("Music Player-Playing")
+        self._model.player.currentMediaChanged.connect(self.update_status)
+        self._model.player.stateChanged.connect(update_title);
+        
+        self._player = MediaPlayerWidget()
         self._layout = QtWidgets.QVBoxLayout();
         self._layout.addWidget(self._menu);
         self._layout.addWidget(self._player);
@@ -168,3 +201,6 @@ class MainWindow(QtWidgets.QWidget):
         self._layout.addWidget(self._status);
 
         self.setLayout(self._layout);
+    def update_status(self):
+        media = self._model.get_current_media_data();
+        self._status.showMessage("%s-%s-%s" % (media.title,media.artist,media.album))
--- Model.py
+++ Model.py
@@ -1,29 +1,73 @@
 from PySide2 import QtMultimedia, QtCore
+from tinytag import TinyTag
+from PySide2.QtMultimedia import QMediaContent
+
+class AudioMetadata():
+    def __init__(self,file_path):
+        tag = TinyTag.get(file_path);
+        self.album = tag.album
+        self.artist = tag.artist
+        self.title = tag.title
+        self.duration = tag.duration
+
 class Model(QtCore.QObject):
     def __init__(self):
         super().__init__();
     
     def init(self):
+        self._playlists_mdata = {} # mdata = meta data
+        self._playlists = {}
+        self._current_playlist = None;
+        self._current_playlist_mdata = None;
+        self._current_playlist_name = None;
         self.player = QtMultimedia.QMediaPlayer(self)
+        self.player.setVolume(50);
         print(self.player.isSeekable());
     
+    def add_playlist(self,name):
+        if name in self._playlists :
+            print("can't add that playlist")
+            return False
+        self._playlists_mdata[name] = [];
+        self._playlists[name] = QtMultimedia.QMediaPlaylist();
+
     def seek(self,p_percent): #position is normalised from 0 to 1
         self.player.setPosition(p_percent * self.player.duration());
 
-    def open_file(self,path):
-        self.player.setMedia(QtCore.QUrl.fromLocalFile(path))
-        self.player.setVolume(50);
+    def add_file(self,path,playlist_name):
+        print(path)
+        n_media = QtMultimedia.QMediaContent(QtCore.QUrl.fromLocalFile(path));
+        self._playlists[playlist_name].addMedia(n_media)
+        self._playlists_mdata[playlist_name].append(AudioMetadata(path));
+        return self._playlists[playlist_name].mediaCount() - 1;
+
+    def open_file(self,index,playlist_name):
+        self._current_playlist= self._playlists[playlist_name]
+        self._current_playlist_mdata = self._playlists_mdata[playlist_name]
+        self._current_playlist_name = playlist_name      
+        self.player.setPlaylist(self._current_playlist);
+        self._current_playlist.setCurrentIndex(index);
         self.player.play()
-        print(self.player.errorString())
-    
+
+    def get_current_media_data(self):
+        return self._current_playlist_mdata[self._current_playlist.currentIndex()];
+    def get_media_data(self,index,playlist_name):
+        a = self._playlists_mdata[playlist_name][index]
+        return a
     def set_volume(self,vol):
         self.player.setVolume(vol);
     def pause(self):
         self.player.pause()
     def play(self):
         self.player.play()
+    def stop(self):
+        self.player.stop()
+    def forwards(self):
+        self._current_playlist.next()
+    def rewind(self):
+        self._current_playlist.previous()
     def get_position(self):
-        if (self.player.duration() == 0) : return 0;
+        if (self.player.duration() == 0) : return None;
         return self.player.position() / self.player.duration();
 
 main_model = Model();
